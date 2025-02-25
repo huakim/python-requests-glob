87be2b0815fb49a6556cbc78622aa9a1ec02e7ee
lskfjwer <uiguruigurovich@gmail.com>
2025-02-25 16:06:06 +0300

    Add descriptions, format code

--- test_setup.py
+++ test_setup.py
@@ -1,9 +1,10 @@
 def test_glob():
-  from requests_glob import GlobAdapter
-  from requests import Session
-  import os
-  s = Session()
-  s.mount('glob://', GlobAdapter(netloc_paths={'.':os.getcwd()}))
-  f=s.get('glob://./globtext*.txt')
-  assert f.text != ''
-  assert f.text == "text\n"
+    from requests_glob import GlobAdapter
+    from requests import Session
+    import os
+
+    s = Session()
+    s.mount("glob://", GlobAdapter(netloc_paths={".": os.getcwd()}))
+    f = s.get("glob://./globtext*.txt")
+    assert f.text != ""
+    assert f.text == "text\n"
